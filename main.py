2290b4642069bfae459aad01003371cdafd3a97a
Patryk Świat <patrykswiat@gmail.com>
2023-11-13 14:23:01 +0100

    add game reseting, add control counter, parse json to dict

--- main.py
+++ main.py
@@ -9,16 +9,32 @@ sio = socketio.AsyncServer(async_mode="aiohttp", cors_allowed_origins="*")
 app = web.Application()
 sio.attach(app)
 
+game = FreeCell(seed=0)
+translator = FreecellTranslator(game)
+i = 0
 
 @sio.event
 async def make_move(sid, data):
+    global translator
+    global game
+    global i
+    
+    i += 1
+    
+    data = json.loads(data)
     move_ml = data.get("move")
 
     print(data)
     print(f"sender: {sid} tells translator to make move:")
     print(f"move_ml: {move_ml}")
-
-    translator.make_move(move_ml)
+    
+    if move_ml is None:
+        print("Resetting game")
+        game = FreeCell(seed=0)
+        translator = FreecellTranslator(game)
+    else:
+        translator.make_move(move_ml)
+    
     state = translator.get_state()
     reward = translator.get_reward()
     moves = translator.get_moves()
@@ -29,12 +45,11 @@ async def make_move(sid, data):
         "game_board": board,
         "reward": reward,
         "state": state.__str__(),
+        "control": i
     }
 
     await sio.emit("get_response", json.dumps(response_data), room=sid)
 
 
 if __name__ == '__main__':
-    game = FreeCell()
-    translator = FreecellTranslator(game)
     web.run_app(app, host="localhost", port=5002)
